6f14cc01e45e7ba12f62317e5d91f5295c741c09
Paulina Kolonczyk <paulinakolonczyk@interia.pl>
2021-03-01 12:23:53 +0100

    Add class active to the clicked link

--- js/script.js
+++ js/script.js
@@ -4,19 +4,22 @@
 //   console.log('links:', links);
 // });
 const titleClickHandler = function(event){
-  console.log('Link was clicked!');
-  console.log(event);
+  const clickedElement = this;
+  console.log('Link was clicked!' , event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
-  /* add class 'active' to the clicked link */
+  /*[IN PROGRESS] add class 'active' to the clicked link */
 
-  /* remove class 'active' from all articles */
+  clickedElement.classList.add('active');
+  console.log('clickedElement:', clickedElement);
+
+  /* [DONE] remove class 'active' from all articles */
 
   const activeArticles = document.querySelectorAll('.post.active');
 
