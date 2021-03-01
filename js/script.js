3d9cb7e5c32ecbde105745e5071affc50752ada8
Paulina Kolonczyk <paulinakolonczyk@interia.pl>
2021-03-01 11:31:06 +0100

    fix remove article

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const titleClickHandler = function(event){
   console.log(event);
 
   /* remove class 'active' from all article links  */
+
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
@@ -16,11 +17,13 @@ const titleClickHandler = function(event){
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.post .active');
+
+  const activeArticles = document.querySelectorAll('.post.active');
 
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
-    }
+}
+
   /* get 'href' attribute from the clicked link */
 
   /* find the correct article using the selector (value of 'href' attribute) */
